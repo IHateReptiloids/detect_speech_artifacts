67993a6527a1d8e26e837799b820849a4227e76a
Nikita Stepanov <stepanov2000nikita@yandex.ru>
2022-04-23 19:20:03 +0300

    add hydra

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import hydra
+from omegaconf import DictConfig
 import torch
 import torch.nn as nn
 import wandb
@@ -6,37 +8,54 @@ from src.datasets import SSPNetVC
 from src.models import Wav2Vec2Pretrained
 from src.trainers import UnsupervisedFineTuningTrainer
 
+DATASETS = {
+    'ssp_net_vc': SSPNetVC
+}
 
-wandb.init()
+MODELS = {
+    'wav2vec2': Wav2Vec2Pretrained
+}
 
-seed = 8228
-torch.manual_seed(8228)
+OPTS = {
+    'adam': torch.optim.Adam
+}
 
-train_ds = SSPNetVC(data_path='data/ssp/data',
-                    labels_path='data/ssp/train_labels.txt',
-                    target_sr=16_000)
-val_ds = SSPNetVC(data_path='data/ssp/data',
-                  labels_path='data/ssp/val_labels.txt',
-                  target_sr=16_000)
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+@hydra.main(config_path='configs', config_name='config')
+def main(cfg: DictConfig):
+    wandb.init()
 
-model = Wav2Vec2Pretrained('base')
-for p in model.parameters():
-    p.requires_grad_(False)
-model = nn.Sequential(
-    model,
-    nn.Linear(model.num_features, 3)
-)
-opt = torch.optim.Adam(model[1].parameters(), lr=3e-4)
-trainer = UnsupervisedFineTuningTrainer(model, opt, device)
+    torch.manual_seed(cfg.seed)
 
-batch_size = 16
-train_loader = torch.utils.data.DataLoader(train_ds, batch_size=batch_size,
-                                           shuffle=True,
-                                           collate_fn=trainer.collate_fn)
-val_loader = torch.utils.data.DataLoader(val_ds, batch_size=batch_size,
-                                         collate_fn=trainer.collate_fn)
+    train_ds = DATASETS[cfg.train_ds.name](**cfg.train_ds.args)
+    val_ds = DATASETS[cfg.val_ds.name](**cfg.val_ds.args)
 
-wandb.watch((model,), log='all', log_freq=100)
-trainer.train_loop(10, train_loader, val_loader, 'checkpoints/wav2vec2-ssp-net-vc')
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    model = MODELS[cfg.model.name](**cfg.model.args)
+
+    for p in model.parameters():
+        p.requires_grad_(False)
+    model = nn.Sequential(
+        model,
+        nn.Linear(model.num_features, 3)
+    )
+
+    opt = OPTS[cfg.opt.name](model[1].parameters(), **cfg.opt.args)
+    trainer = UnsupervisedFineTuningTrainer(model, opt, device)
+
+    train_loader = torch.utils.data.DataLoader(train_ds,
+                                               batch_size=cfg.batch_size,
+                                               shuffle=True,
+                                               collate_fn=trainer.collate_fn)
+    val_loader = torch.utils.data.DataLoader(val_ds, batch_size=cfg.batch_size,
+                                             collate_fn=trainer.collate_fn)
+
+    wandb.watch((model,), log='all', log_freq=cfg.wandb_log_freq)
+    trainer.train_loop(cfg.num_epochs, train_loader, val_loader,
+                       cfg.checkpoint_dir)
+
+
+if __name__ == '__main__':
+    main()
+
