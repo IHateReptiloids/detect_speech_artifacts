6c8371d0879a04604f5ced4f590ce15e105cf675
Nikita Stepanov <stepanov2000nikita@yandex.ru>
2022-04-26 16:15:12 +0300

    fix bugs and update requirements.txt

--- src/utils/visualize.py
+++ src/utils/visualize.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def visualize(wav, pred, y_aligned, ind2label):
-    if not wav.dim() == pred.dim() == y_aligned.dim() == 1:
+    if not wav.ndim == pred.ndim == y_aligned.ndim == 1:
         raise ValueError('All arguments must be one-dimensional')
     if len(pred) != len(y_aligned):
         raise ValueError('pred and y_aligned must have same length')
@@ -13,20 +13,22 @@ def visualize(wav, pred, y_aligned, ind2label):
     colors = ['blue', 'green', 'red', 'cyan',
               'magenta', 'yellow', 'black']
     fig = plt.figure(figsize=(20, 5), dpi=100)
+
+    part_size = len(wav) // len(pred)
     for i in range(len(pred)):
-        wav_part = wav[len(wav) // len(pred) * i:len(wav) // len(pred) * (i + 1)]
+        wav_part = wav[part_size * i:part_size * (i + 1)]
         if pred[i] != y_aligned[i]:
             plt.scatter(
-                len(wav) // len(pred) * (i + 0.5) / 16_000,
+                part_size * (i + 0.5) / 16_000,
                 0,
-                c=colors[pred[i].item()],
+                c=colors[pred[i]],
                 zorder=1,
                 s=10
             )
         plt.plot(
-            np.arange(len(wav) // len(pred) * i, len(wav) // len(pred) * (i + 1)) / 16_000,
+            np.arange(part_size * i, part_size * (i + 1)) / 16_000,
             wav_part,
-            colors[y_aligned[i].item()],
+            colors[y_aligned[i]],
             zorder=0
         )
     for i in range(len(ind2label)):
@@ -39,6 +41,7 @@ def visualize(wav, pred, y_aligned, ind2label):
     fig.savefig(io_buf, format='raw', dpi=100)
     io_buf.seek(0)
     img_arr = np.reshape(np.frombuffer(io_buf.getvalue(), dtype=np.uint8),
-                         newshape=(int(fig.bbox.bounds[3]), int(fig.bbox.bounds[2]), -1))
+                         newshape=(int(fig.bbox.bounds[3]),
+                         int(fig.bbox.bounds[2]), -1))
     io_buf.close()
     return img_arr
